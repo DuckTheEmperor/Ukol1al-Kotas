6ff4174bc8afbf55eba84e7d4a863af96035f47f
Ducky <159123765+DuckTheEmperor@users.noreply.github.com>
2024-11-06 09:27:48 +0000

    final_Kotas

--- jp_Kotas.py
+++ jp_Kotas.py
@@ -1,1 +1,13 @@
-arra
+#1
+nebe = ["slunce", "mesic", "nebe", "mrak", "strom"]
+#2
+nebe.append("vitr")
+#3
+nebe.remove("mesic")
+#4
+zeme = [a.replace("vitr", "slunce") for a in nebe]
+#5
+pridat = ["list", "zem"]
+zeme.extend (pridat)
+#6
+

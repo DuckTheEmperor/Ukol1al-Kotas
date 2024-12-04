b4478009db5ff49a9dbebb4ee7e2862c1c5951a6
Ducky <159123765+DuckTheEmperor@users.noreply.github.com>
2024-12-04 10:27:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 import random
-#1.
-array1 = [43, 93, 10, 53, 71, 4, 81, 46, 51] 
+import bubble_sort
+array = [4, 87, 31, 70, 96, 11, 2, 42, 63, 28]
 
-#2.
-sp = sorted(array1)
-ph = sp[len(sp) // 2]
-max = max(array1)
-min = min(array1)
-print(min, ph, max)
+def bubble_sort():
+    n=len(array)
+    for i in range(n-1):
+        for j in range (0, n-i-1):
+            if array[j]>array[j+1]:
+                array[j], array[j+1]=array[j+1], array[j]
+    return array
 
-#3.
-array1[6] = 34
+print(bubble_sort())
 
-#4. 
-print(array1[8])
+arrayb = [88, 76, 3, 90, 21, 10, 1, 65, 43, 28]
+def is_sorted(arrayb):
+    for i in range(1, len(arrayb)):
+        if arrayb[i] < arrayb[i-1]:
+            return False
+    return True
 
-#5.
-print (len(array1))
+def bogosort(arrayb):
+    count = 0
+    while not is_sorted(arrayb):
+        random.shuffle(arrayb)
+        count += 1
+        print(f"Pokus {count}: {arrayb}")
+    print(f"seznam serazen po {count} pokusech")
 
-#6.
-print(min, "a", max)
+bogosort(arrayb)
 
-#7.
-array2 = [7, 14, 75, 43, 98, 23]
+print("Bogo", arrayb)
 
-#8.
-print (sum(array2))
+array = [45, 56, 47, 64, 8, 68, 75, 24, 2, 68]
 
-#9.
-print(array2[0] + array2[5])
+def selectionSort(array, size):
+    
+    for ind in range(size):
+        min_index = ind
+ 
+        for j in range(ind + 1, size):
+         
+            if array[j] < array[min_index]:
+                min_index = j
+        
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+ 
+arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
+size = len(arr)
+selectionSort(arr, size)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
